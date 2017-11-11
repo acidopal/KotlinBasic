00ad9ee6ab13681aedcbf69ff1bdad76b6b32360
Naufal Rasyid <naufalrasyid86@gmail.com>
2017-11-11 07:18:49 +0700

    Fix inputs

--- ConvertDataType.kt
+++ ConvertDataType.kt
@@ -4,6 +4,7 @@ fun main(args:Array<String>){
     var n2Str:String="12"
     n2=n2Str.toInt()
     var n2Float:Float?
+
     //konvert String to Float
     n2Float=n2Str.toFloat()
     println("n1 : "+ n1)
@@ -11,6 +12,7 @@ fun main(args:Array<String>){
     println("n2Float : "+ n2Float)
     var xpi:Double=3.14
     println("xpi : "+xpi)
+
     //Konvert Double to Integer
     var IntPi:Int=xpi.toInt()
     print("IntPi : "+IntPi)
