76b24c706da8f5d338b07a50b79573e5d4401c11
acidopal <im.acidopal@gmail.com>
2017-11-11 08:48:30 +0700

    Add operations

---
+++ Operations.kt
@@ -0,0 +1,13 @@
+/*
+Math calculator
+ */
+fun main(args:Array<String>){
+    println("--- Math Calculator ---")
+    print("Nilai 1 : ")
+    var n1:Int= readLine()!!.toInt()
+    print("Nilai 2 : ")
+    var n2:Int=20
+    var sum:Int?
+    sum=n1+n2
+    println("Sum : "+sum)
+}
