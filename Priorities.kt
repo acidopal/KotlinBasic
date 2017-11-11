322d74e15feb1d77f0519e35d9ed3943d30e4c4a
acidopal <im.acidopal@gmail.com>
2017-11-11 09:05:26 +0700

    Description priorities

--- Priorities.kt
+++ Priorities.kt
@@ -11,6 +11,9 @@ fun main(args:Array<String>){
     var n2:Int=20
     var n3:Int=5
     var sum:Int?
+    // Perkalian yg pertama kali dihitung
+    // n2=20 *  3 = 60
+    // n1=10 + 60 = 70 -3 = 67
     sum=n1+n2*3-3
     print("Sum : $sum")
 }
