26f830a57ab4c1ea9abba74b3c02d42483306fd4
acidopal <im.acidopal@gmail.com>
2017-11-11 09:22:00 +0700

    Add more logic condition

---
+++ LogicCondition.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>){
+    //Condition TRUE
+    //Karena 3 kurang dari 6
+    println(3<6)
+
+    //Condition FALSE
+    //Karena kondisi 3 < 10 True , dan & > 10 False
+    //True + False = False
+    println(3<10 && 8>10)
+}
