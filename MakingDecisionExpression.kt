fe929da53cea41f803e27e783a88a3492b72f799
acidopal <im.acidopal@gmail.com>
2017-11-12 20:19:49 +0700

    MakingDecisionExpression with Kotlin

---
+++ MakingDecisionExpression.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>){
+    var n1=10
+    var n2=20
+    var n3=30
+    var max:Int?
+    if (n1>n2){
+        max=n1
+        print("max : "+n1)
+    }else{
+        max=n2
+        print("max : "+n2)
+    }
+}
