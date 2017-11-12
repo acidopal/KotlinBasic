4392c4d17bb583604e2748763ac80cd389630fb4
acidopal <im.acidopal@gmail.com>
2017-11-12 20:23:07 +0700

    Step 2 MakinDecisionExpression with kotlin

--- MakingDecisionExpression.kt
+++ MakingDecisionExpression.kt
@@ -2,12 +2,16 @@ fun main(args:Array<String>){
     var n1=10
     var n2=20
     var n3=30
-    var max:Int?
-    if (n1>n2){
-        max=n1
-        print("max : "+n1)
-    }else{
-        max=n2
-        print("max : "+n2)
-    }
+    var max=if (n1>n2) n1 else n2
+    print("max : $max")
+    /* ---- Cara #1
+        var max:Int?
+        if (n1>n2){
+            max=n1
+            print("max : "+n1)
+        }else{
+            max=n2
+            print("max : "+n2)
+        }
+    */
 }
